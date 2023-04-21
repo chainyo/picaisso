4892195b82459f7a71af068af905aa9fbcab26d7
chainyo <thomas@deeptools.ai>
2023-04-21 07:08:38 +0000

    fix sync tree.command on guild join

--- picaisso/bot/main.py
+++ picaisso/bot/main.py
@@ -21,7 +21,7 @@ class OpenjourneyBot(discord.Client):
         *args,
         web_client: ClientSession,
         intents: Optional[discord.Intents] = None,
-    ):
+    ) -> None:
         if intents is None:
             intents = discord.Intents.default()
         intents.members = True
@@ -31,12 +31,23 @@ class OpenjourneyBot(discord.Client):
         self.tree = app_commands.CommandTree(self)
         
     
-    async def on_ready(self):
+    async def on_ready(self) -> None:
+        """When the bot is ready."""
         await self.wait_until_ready()
         logger.debug(f"Logged in as {self.user}")
+
+    async def on_guild_join(self, guild: discord.Guild) -> None:
+        """
+        On guild join.
+
+        Args:
+            guild (discord.Guild): The guild that the bot joined.
+        """
+        await self.tree.sync(guild=guild)
         
     
-    async def _authenticate(self):
+    async def _authenticate(self) -> None:
+        """Authenticate with the API."""
         async with self.web_client.post(
             f"{os.getenv('API_URL')}/auth",
             headers={
@@ -56,13 +67,24 @@ class OpenjourneyBot(discord.Client):
             })
         
     
-    async def setup_hook(self):
+    async def setup_hook(self) -> None:
+        """Setup the bot at startup."""
         await self._authenticate()
         self.tree.add_command(art)
         await self.tree.sync()
 
 
-    async def generate_art(self, interaction: discord.Interaction, prompt: str):
+    async def generate_art(self, interaction: discord.Interaction, prompt: str) -> None:
+        """
+        Generate art from prompt and send it to the user.
+
+        Args:
+            interaction (discord.Interaction): The interaction that triggered the command.
+            prompt (str): The prompt to generate art from.
+
+        Raises:
+            Exception: If something went wrong while generating art.
+        """
         try:
             async with self.web_client.post(
                 f"{os.getenv('API_URL')}/generate",
@@ -80,10 +102,17 @@ class OpenjourneyBot(discord.Client):
 
 
 @app_commands.command(name="art", description="Generate art")
-async def art(
-    interaction: discord.Interaction,
-    prompt: str,
-):
+async def art(interaction: discord.Interaction, prompt: str) -> None:
+    """
+    Command to generate art from prompt for the user.
+
+    Args:
+        interaction (discord.Interaction): The interaction that triggered the command.
+        prompt (str): The prompt to generate art from.
+
+    Raises:
+        requests.exceptions.HTTPError: If something went wrong while generating art.
+    """
     try:
         if prompt != "":
             await interaction.response.send_message(f"{interaction.user} requested: `{prompt}`")
@@ -97,6 +126,7 @@ async def art(
   
    
 async def main():
+    """Main loop."""""
     async with ClientSession() as session:
         async with OpenjourneyBot(commands.when_mentioned, web_client=session) as client:
             await client.start(os.getenv("DISCORD_TOKEN"))
