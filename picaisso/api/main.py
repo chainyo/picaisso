cc4499e407a97015726540dfb4594902d5b85246
Tom Aarsen <Cubiegamedev@gmail.com>
2023-05-03 20:35:21 +0200

    Use port 7681 to add Windows support
    Also, mention that on Windows you can/should use host.docker.internal instead of 'localhost'

--- picaisso/api/main.py
+++ picaisso/api/main.py
@@ -111,4 +111,4 @@ async def authentication(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=7680, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7681, reload=True)
